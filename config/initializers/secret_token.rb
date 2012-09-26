cf1d0be23358647d9f073376bd9b3abb9a27e1ac
Oleg Trofimov <otrofimo@gmail.com>
2012-09-26 11:38:32 -0700

    lets do this, step 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clowncounter::Application.config.secret_token = '3075dba2215fc50f1c8b0fc49288cad4fd72b85363cbec0b4054cf91fbd92e7ee95c4274df9a1a732b837084016e1259dfe09081c934720b64be78cf0d1940ab'
